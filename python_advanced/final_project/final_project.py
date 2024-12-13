e168806179c76dcd0d67e107489e00f483208438
danielsaiger1 <daniel.saiger@yahoo.de>
2024-12-13 11:05:24 +0100

    data loading

--- python_advanced/final_project/final_project.py
+++ python_advanced/final_project/final_project.py
@@ -1,1 +1,17 @@
 import pandas as pd
+import numpy as np
+from numpy import loadtxt
+
+input_path = "input_data/"
+file_names = [
+    "ce", "cp", "eps1", "se", "vs1", 
+    "fs1", "fs2", 
+    "ps1", "ps2", "ps3", "ps4", "ps5", "ps6",
+    "ts1", "ts2", "ts3", "ts4"
+]
+
+data = {}
+
+for file_name in file_names:
+    data[file_name] = loadtxt(f"{input_path}{file_name}.txt")
+    
