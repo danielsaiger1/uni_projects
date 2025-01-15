52b48242eb8f0cc881c0d3dd8a02893884656bc3
Daniel Saiger <daniel.saiger@yahoo.de>
2025-01-15 18:05:38 +0100

    change to run correctly

--- web_scraper/data_processor.py
+++ web_scraper/data_processor.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 import json
+
 class Processor:
     def __init__(self, input_path):
         self.input_path = input_path
     
     def load_data(self):
         today = datetime.today().strftime('%Y%m%d')
-        with open(f"{self.input_path}/{today}_output_data.json", 'r') as f:
-            self.data = json.load(f)
-            return self.data
+        try:
+            with open(f"{self.input_path}/{today}_output_data.json", 'r') as f:
+                self.data = json.load(f)
+                print("Data loaded successfully")
+                return self.data
+        except Exception as e:
+            print(f"Data not found. Check {e}")
+            return None
+        
+def main():
+    processor = Processor('./output')
+    data = processor.load_data()
 
-processor = Processor('./output')
-
-data = processor.load_data()
-
-print(data)
+if __name__ == "__main__":
+    main()
